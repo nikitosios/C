1cd80924001231b4bf24607a6366069327e497b3
nikitosios <nikita.diachina@yandex.ru>
2017-02-04 23:14:46 +0200

    update

--- openGL/main.c
+++ openGL/main.c
@@ -7,66 +7,55 @@ double rotate_x = 0, rotate_y = 0;
 void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	// Сброс трансформаций
 	glLoadIdentity();
+	glRotatef(rotate_x, 1.0, 0.0, 0.0);
+	glRotatef(rotate_y, 0.0, 1.0, 0.0);
 
-	// Вращение при изменении пользователем значений rotate_x и rotate_y
-	glRotatef( rotate_x, 1.0, 0.0, 0.0 );
-	glRotatef( rotate_y, 0.0, 1.0, 0.0 );
-
-	// Многоцветная сторона - ПЕРЕДНЯЯ
 	glBegin(GL_POLYGON);
-
-	glColor3f( 1.0, 0.0, 0.0 );     glVertex3f(  0.5, -0.5, -0.5 );      // P1 is red
-	glColor3f( 0.0, 1.0, 0.0 );     glVertex3f(  0.5,  0.5, -0.5 );      // P2 is green
-	glColor3f( 0.0, 0.0, 1.0 );     glVertex3f( -0.5,  0.5, -0.5 );      // P3 is blue
-	glColor3f( 1.0, 0.0, 1.0 );     glVertex3f( -0.5, -0.5, -0.5 );      // P4 is purple
-
+	glColor3f(1.0, 0.0, 0.0); glVertex3f(0.5, -0.5, -0.5);
+	glColor3f(0.0, 1.0, 0.0); glVertex3f(0.5, 0.5, -0.5);
+	glColor3f(0.0, 0.0, 1.0); glVertex3f(-0.5, 0.5, -0.5);
+	glColor3f(1.0, 0.0, 1.0); glVertex3f(-0.5, -0.5, -0.5);
 	glEnd();
 
-	// Белая сторона - ЗАДНЯЯ
 	glBegin(GL_POLYGON);
-	glColor3f(   1.0,  1.0, 1.0 );
-	glVertex3f(  0.5, -0.5, 0.5 );
-	glVertex3f(  0.5,  0.5, 0.5 );
-	glVertex3f( -0.5,  0.5, 0.5 );
-	glVertex3f( -0.5, -0.5, 0.5 );
+	glColor3f(1.0, 1.0, 1.0);
+	glVertex3f(0.5, -0.5, 0.5);
+	glVertex3f(0.5, 0.5, 0.5);
+	glVertex3f(-0.5, 0.5, 0.5);
+	glVertex3f( -0.5, -0.5, 0.5);
 	glEnd();
 
-	// Фиолетовая сторона - ПРАВАЯ
 	glBegin(GL_POLYGON);
-	glColor3f(  1.0,  0.0,  1.0 );
-	glVertex3f( 0.5, -0.5, -0.5 );
-	glVertex3f( 0.5,  0.5, -0.5 );
-	glVertex3f( 0.5,  0.5,  0.5 );
-	glVertex3f( 0.5, -0.5,  0.5 );
+	glColor3f(1.0, 0.0, 1.0);
+	glVertex3f(0.5, -0.5, -0.5);
+	glVertex3f(0.5, 0.5, -0.5);
+	glVertex3f(0.5, 0.5, 0.5);
+	glVertex3f(0.5, -0.5, 0.5);
 	glEnd();
 
-	// Зеленая сторона - ЛЕВАЯ
 	glBegin(GL_POLYGON);
-	glColor3f(   0.0,  1.0,  0.0 );
-	glVertex3f( -0.5, -0.5,  0.5 );
-	glVertex3f( -0.5,  0.5,  0.5 );
-	glVertex3f( -0.5,  0.5, -0.5 );
-	glVertex3f( -0.5, -0.5, -0.5 );
+	glColor3f(0.0, 1.0, 0.0);
+	glVertex3f(-0.5, -0.5, 0.5);
+	glVertex3f(-0.5, 0.5, 0.5);
+	glVertex3f(-0.5, 0.5, -0.5);
+	glVertex3f(-0.5, -0.5, -0.5);
 	glEnd();
 
-	// Синяя сторона - ВЕРХНЯЯ
 	glBegin(GL_POLYGON);
-	glColor3f(   0.0,  0.0,  1.0 );
-	glVertex3f(  0.5,  0.5,  0.5 );
-	glVertex3f(  0.5,  0.5, -0.5 );
-	glVertex3f( -0.5,  0.5, -0.5 );
-	glVertex3f( -0.5,  0.5,  0.5 );
+	glColor3f(0.0, 0.0, 1.0);
+	glVertex3f(0.5, 0.5, 0.5);
+	glVertex3f(0.5, 0.5, -0.5);
+	glVertex3f(-0.5, 0.5, -0.5);
+	glVertex3f(-0.5, 0.5, 0.5);
 	glEnd();
 
-	// Красная сторона - НИЖНЯЯ
 	glBegin(GL_POLYGON);
-	glColor3f(   1.0,  0.0,  0.0 );
-	glVertex3f(  0.5, -0.5, -0.5 );
-	glVertex3f(  0.5, -0.5,  0.5 );
-	glVertex3f( -0.5, -0.5,  0.5 );
-	glVertex3f( -0.5, -0.5, -0.5 );
+	glColor3f(1.0, 0.0, 0.0);
+	glVertex3f(0.5, -0.5, -0.5);
+	glVertex3f(0.5, -0.5, 0.5);
+	glVertex3f(-0.5, -0.5, 0.5);
+	glVertex3f(-0.5, -0.5, -0.5);
 	glEnd();
 
 	glFlush();
@@ -76,21 +65,15 @@ void display(void)
 
 void specialKeys(int key, int x, int y)
 {
-	//  Правая стрелка - увеличение вращения на 5 градусов
 	if (key == GLUT_KEY_RIGHT)
 		rotate_y += 5;
-
-	//  Левая стрелка - уменьшение вращения на 5 градусов
 	else if (key == GLUT_KEY_LEFT)
 		rotate_y -= 5;
-
 	else if (key == GLUT_KEY_UP)
 		rotate_x += 5;
-
 	else if (key == GLUT_KEY_DOWN)
 		rotate_x -= 5;
 
-	//  Запрос обновления экрана
 	glutPostRedisplay();
 	return;
 }
