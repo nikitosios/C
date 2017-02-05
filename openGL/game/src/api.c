2c5c78db65d95adee3ca9c5ad383f918b9d1d4bc
nikitosios <nikita.diachina@yandex.ru>
2017-02-05 09:56:54 +0200

    game

--- openGL/game/src/api.c
+++ openGL/game/src/api.c
@@ -1,45 +1,47 @@
 void drawCube(float x, float y, float z, float a)
 {
-	glBegin(GL_POLYGON);
-	glVertex3f(x, y - a, z - a);
-	glVertex3f(x, y, z - a);
-	glVertex3f(x - a, y, z - a);
-	glVertex3f(x - a, y - a, z - a);
-	glEnd();
+	unsigned short xr = x + a, yd = y + a, zf = z + a;
 
 	glBegin(GL_POLYGON);
-	glVertex3f(x, y - a, z);
+	glVertex3f(xr, y, z);
+	glVertex3f(xr, yd, z);
+	glVertex3f(x, yd, z);
 	glVertex3f(x, y, z);
-	glVertex3f(x - a, y, z);
-	glVertex3f(x - a, y - a, z);
 	glEnd();
 
 	glBegin(GL_POLYGON);
-	glVertex3f(x, y - a, z - a);
-	glVertex3f(x, y, z - a);
+	glVertex3f(xr, y, zf);
+	glVertex3f(xr, yd, zf);
+	glVertex3f(x, yd, zf);
+	glVertex3f(x, y, zf);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex3f(xr, y, z);
+	glVertex3f(xr, yd, z);
+	glVertex3f(xr, yd, zf);
+	glVertex3f(xr, y, zf);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex3f(x, y, zf);
+	glVertex3f(x, yd, zf);
+	glVertex3f(x, yd, z);
 	glVertex3f(x, y, z);
-	glVertex3f(x, y - a, z);
 	glEnd();
 
 	glBegin(GL_POLYGON);
-	glVertex3f(x - a, y - a, z);
-	glVertex3f(x - a, y, z);
-	glVertex3f(x - a, y, z -  a);
-	glVertex3f(x - a, y - a, z - a);
+	glVertex3f(xr, yd, zf);
+	glVertex3f(xr, yd, z);
+	glVertex3f(x, yd, z);
+	glVertex3f(x, yd, zf);
 	glEnd();
 
 	glBegin(GL_POLYGON);
+	glVertex3f(xr, y, z);
+	glVertex3f(xr, y, zf);
+	glVertex3f(x, y, zf);
 	glVertex3f(x, y, z);
-	glVertex3f(x, y, z - a);
-	glVertex3f(x - a, y, z - a);
-	glVertex3f(x - a, y, z);
-	glEnd();
-
-	glBegin(GL_POLYGON);
-	glVertex3f(x, y - a, z - a);
-	glVertex3f(x, y - a, z);
-	glVertex3f(x - a, y - a, z);
-	glVertex3f(x - a, y - a, z - a);
 	glEnd();
 
 	return;
