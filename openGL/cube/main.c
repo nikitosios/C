1fe1b281f2e9d8c14fb554ad060de74ba324d677
nikitosios <nikita.diachina@yandex.ru>
2017-02-05 23:28:36 +0200

    update openGL

--- openGL/cube/main.c
+++ openGL/cube/main.c
@@ -1,4 +1,4 @@
-/* File for "Lighting" lesson of the OpenGL tutorial on
+/* file for "Lighting" lesson of the OpenGL tutorial on
  * www.videotutorialsrock.com
  */
 
@@ -6,18 +6,18 @@
 
 unsigned short window_width = 800, window_height = 600;
 
-//Initializes 3D rendering
+/* initializes 3D rendering */
 void init3D(void) {
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_COLOR_MATERIAL);
-	glEnable(GL_LIGHTING); //Enable lighting
-	glEnable(GL_LIGHT0); //Enable light #0
-	glEnable(GL_LIGHT1); //Enable light #1
-	glEnable(GL_NORMALIZE); //Automatically normalize normals
-	//glShadeModel(GL_SMOOTH); //Enable smooth shading
+	glEnable(GL_LIGHTING); /* enable lighting */
+	glEnable(GL_LIGHT0); /* enable light #0 */
+	glEnable(GL_LIGHT1); /* enable light #1 */
+	glEnable(GL_NORMALIZE); /* automatically normalize normals */
+	/* glShadeModel(GL_SMOOTH); */ /* enable smooth shading */
 }
 
-//Called when the window is resized
+/* called when the window is resized */
 void handleResize(int w, int h) {
 	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);
@@ -25,30 +25,29 @@ void handleResize(int w, int h) {
 	gluPerspective(45.0, (double)w / (double)h, 1.0, 200.0);
 }
 
-float _angle = -70.0f;
+float _angle = -70.0;
 
-//Draws the 3D scene
-void drawScene() {
+/* draws the 3D scene */
+void drawScene(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	
 	glTranslatef(0.0f, 0.0f, -8.0f);
 	
-	//Add ambient light
-	GLfloat ambientColor[] = {0.2f, 0.2f, 0.2f, 1.0f}; //Color (0.2, 0.2, 0.2)
+	/* add ambient light */
+	GLfloat ambientColor[] = {0.2f, 0.2f, 0.2f, 1.0f}; /* Color (0.2, 0.2, 0.2) */
 	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientColor);
 	
-	//Add positioned light
-	GLfloat lightColor0[] = {0.5f, 0.5f, 0.5f, 1.0f}; //Color (0.5, 0.5, 0.5)
-	GLfloat lightPos0[] = {4.0f, 0.0f, 8.0f, 1.0f}; //Positioned at (4, 0, 8)
+	/* add positioned light */
+	GLfloat lightColor0[] = {0.5f, 0.5f, 0.5f, 1.0f}; /* color (0.5, 0.5, 0.5) */
+	GLfloat lightPos0[] = {4.0f, 0.0f, 8.0f, 1.0f}; /* positioned at (4, 0, 8) */
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor0);
 	glLightfv(GL_LIGHT0, GL_POSITION, lightPos0);
 	
-	//Add directed light
-	GLfloat lightColor1[] = {0.5f, 0.2f, 0.2f, 1.0f}; //Color (0.5, 0.2, 0.2)
-	//Coming from the direction (-1, 0.5, 0.5)
+	/* add directed light */
+	GLfloat lightColor1[] = {0.5f, 0.2f, 0.2f, 1.0f}; /* color (0.5, 0.2, 0.2) */
+	/* coming from the direction (-1, 0.5, 0.5) */
 	GLfloat lightPos1[] = {-1.0f, 0.5f, 0.5f, 0.0f};
 	glLightfv(GL_LIGHT1, GL_DIFFUSE, lightColor1);
 	glLightfv(GL_LIGHT1, GL_POSITION, lightPos1);
