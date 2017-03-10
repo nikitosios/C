d7d4445315bb2ee9767460ef950bebff3dcaee40
nikitosios <nikita.diachina@yandex.ru>
2017-03-10 21:59:46 +0200

    update

--- openGL/sphere/main.c
+++ openGL/sphere/main.c
@@ -1,11 +1,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 #define MATH_3D_IMPLEMENTATION
 #include "math_3d.h"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "shaders.h"
+
+float angle = 0.0f;
+float pos = 0.0f;
+char op = 0;
+
+void timer (void)
+{
+	if (op)
+		pos += 0.01f;
+	else
+		pos -= 0.01f;
+	if (pos >= 2.0f)
+		op = 0;
+	if (pos <= -2.0f)
+		op = 1;
+	angle += 1.0f;
+	if (angle >= 360.0f)
+		angle = 0.0f;
+	return;
+}
 
 int main (void)
 {
@@ -28,12 +49,13 @@ int main (void)
 	GLuint shaders[] = {vertShader, fragShader};
 	prepareProgram(&programD, shaders, 2);
 
-	int resolution = 100;
+	int resolution = 50;
 	float radius = 0.5f;
 	float x1, y1, z1, x2, y2, z2;
 	float inc1, inc2, inc3, inc4, inc5, radius1, radius2;
 
-	vec3_t vertexes[resolution * resolution * 6];
+	vec3_t * vertexes;
+	vertexes = malloc(resolution * resolution * 6 * sizeof(vec3_t));
 
 	int i = 0;
 	for (int w = 0; w < resolution; ++w)
@@ -65,6 +87,20 @@ int main (void)
 		}
 	}
 
+	vec3_t * colors;
+	colors = malloc(resolution * resolution * 6 * sizeof(vec3_t));
+
+	float r, g, b;
+
+	srand(time(NULL));
+	for (int i = 0; i < resolution * resolution * 6; ++i)
+	{
+		r = rand() % 1000 / 1000.0f;
+		g = rand() % 1000 / 1000.0f;
+		b = rand() % 1000 / 1000.0f;
+		colors[i] = vec3(r, g, b);
+	}
+
 	GLuint vertexattributes;
 	glGenVertexArrays(1, &vertexattributes);
 	glBindVertexArray(vertexattributes);
@@ -72,21 +108,63 @@ int main (void)
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof vertexes, vertexes, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER,
+			resolution * resolution * 6 * sizeof(vec3_t), vertexes,
+			GL_STATIC_DRAW);
 
+	GLuint colorbuffer;
+	glGenBuffers(1, &colorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glBufferData(GL_ARRAY_BUFFER,
+			resolution * resolution * 6 * sizeof(vec3_t), colors,
+			GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
+			3 * sizeof(float), (void*)0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE,
 			3 * sizeof(float), (void*)0);
 
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LESS);
 
+	double time, lastTime;
+	mat4_t projection, view, model, MVP;
+
+	lastTime = glfwGetTime();
 	while (!glfwWindowShouldClose(window))
 	{
+		time = glfwGetTime();
+		if (time - lastTime >= 0.05)
+		{
+			timer();
+			lastTime = time;
+		}
+
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(programD);
 
+		unsigned int MVPpos;
+		MVPpos = glGetUniformLocation(programD, "MVP");
+
+		mat4_t rotation, translation, scaling, model;
+		MVP = m4_perspective(45.0f, 1024.0f / 768.0f, 0.1f, 100.0f);
+		MVP = m4_mul(MVP, m4_look_at(vec3(0.0f, 0.0f, 3.0f),
+					vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f)));
+		scaling = m4_scaling(vec3(1.0f, 1.0f, 1.0f));
+		rotation = m4_rotation(angle, vec3(0.0, 0.0, 1.0));
+		translation = m4_translation(vec3(pos, 0.0, 0.0));
+		model = m4_mul(translation, m4_mul(rotation, scaling));
+		MVP = m4_mul(MVP, model);
+		
+		glUniformMatrix4fv(MVPpos, 1, GL_FALSE, (float *) &MVP);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 		glDrawArrays(GL_TRIANGLES, 0, resolution * resolution * 6 * 3);
 
 		glfwSwapBuffers(window);
--- openGL/sphere/shaders/vertex.vsh
+++ openGL/sphere/shaders/vertex.vsh
@@ -1,10 +1,13 @@
 #version 330
 
 layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 vertexColor;
+
+uniform mat4 MVP;
 
 out vec3 color;
 
 void main() {
-	color = vec3(1.0, 0.0, 0.0);
-	gl_Position = vec4(position, 1.0);
+	color = vertexColor;
+	gl_Position = MVP * vec4(position, 1.0);
 }
