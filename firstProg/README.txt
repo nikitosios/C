6db84e04b6e29b163583639b8b9367bf50e7cc40
Nikitosios <nikita.diachina@yandex.ru>
2017-09-13 21:32:19 +0300

    update

--- firstProg/README.txt
+++
@@ -1,6 +0,0 @@
-
-Hello!
-
-It is a blank for program.
-I'm making one program but this blank is not needed.
-
--- parser/src/main.c
+++ parser/src/main.c
@@ -7,26 +7,40 @@
 
 #define BUFLEN 1000000
 
-void * buffer;
-size_t buflen, n;
-
 int main (void)
 {
-    buffer = malloc(BUFLEN);
+    void * buffer;
+    size_t buflen, n;
+    curl_socket_t sockfd;
+    CURLcode res;
+    CURL * curl;
+
     buflen = BUFLEN;
+    buffer = malloc(buflen);
 
     printf("Parser started.\n");
-    CURL * curl = curl_easy_init();
+    curl = curl_easy_init();
     if (curl) {
-        CURLcode res;
         curl_easy_setopt(curl, CURLOPT_URL, "https://github.com");
         curl_easy_setopt(curl, CURLOPT_CONNECT_ONLY, 1);
+
         res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            printf("Error: %s\n", curl_easy_strerror(res));
+            return 1;
+        }
+
+        res = curl_easy_getinfo(curl, CURLINFO_ACTIVESOCKET, &sockfd);
+        if (res != CURLE_OK) {
+            printf("Error: %s\n", curl_easy_strerror(res));
+            return 1;
+        }
+
         curl_easy_recv(curl, buffer, buflen, &n);
         curl_easy_cleanup(curl);
     }
 
-    printf("%s\n", (char *)buffer);
+    printf("%i bytes recieved.\n%s\n", n, (char *)buffer);
     free(buffer);
     return 0;
 }
